64c4596317222583f7acaac6417b6a3af3c5c8b4
董赟 <45852446+DongYun666@users.noreply.github.com>
2023-10-09 21:26:45 +0800

    Update WaveTransformer3.py

--- models/WaveTransformer3.py
+++ models/WaveTransformer3.py
@@ -9,52 +9,12 @@ from models.MLP import MLP
 from models.encoder import TrafficADEncoder, TrafficADEncoderLayer
 from models.embed import DataEmbedding
 
-
+# 为实现
 class WaveletADFormer3(nn.Module):
     def __init__(self,attention_type, feature_num, e_layers, num_heads, num_classes, win_size, device, dropout = 0.1,d_model = 512, wavelet_method = 'db4',trainable = True):
         super(WaveletADFormer3, self).__init__()
 
-        self.enc_embedding = DataEmbedding(feature_num, d_model, device, dropout)
-
-        self.encoder = TrafficADEncoder(
-            [
-                TrafficADEncoderLayer(
-            
-                    WaveletAttention(
-            
-                        FullAttention() if attention_type == 'full' else CosinAttention(num_heads=num_heads,device=device),
-                        L = win_size,
-                        num_heads = num_heads,
-                        wavename = wavelet_method,
-                        d_model = d_model,
-                        device = device,
-                        trainable = trainable
-                    ),
-                    d_model=d_model,
-                    dropout=dropout
-                ) for i in range(e_layers)
-            ],
-        )
-
-        self.norm = nn.LayerNorm(d_model)
-
-        self.avgpool = nn.AdaptiveAvgPool1d(1)
-
-        self.head = MLP(d_model, num_classes, dropout)
     def forward(self, x):
-
-        # position embedding
-        x = self.enc_embedding(x)     # B W D
-
-        encoder_out = self.encoder(x)             
-
-        out = self.norm(encoder_out)   # B W D
-
-        out = self.avgpool(out.transpose(1,2))  # 32 128 1
-
-        out = torch.flatten(out,1) # 32 128
-
-        out = self.head(out)
 
         return out
 
